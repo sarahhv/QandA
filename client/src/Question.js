964e50b5ee3374318b770e55a4f41901cd995d0c
Sarah <eaaskh@students.eaaa.dk>
2020-04-30 11:04:36 +0200

    Trying reload question

--- client/src/Question.js
+++ client/src/Question.js
@@ -28,29 +28,53 @@ class Question extends Component {
 
     render() {
         const question = this.props.getQuestion(this.props.id);
-        const mapFunction = (answer, index) =>
+/*        const mapFunction = (answer, index) =>
             <li key={index}>
                 {answer.text}
                 <button onClick={() => this.upVote(answer._id, question)} style={{marginLeft: "2vw"}}>Up</button>
                 <span style={{margin: "0 2vw"}}>{answer.vote}</span>
                 <button onClick={() => this.downVote(answer._id, question)}>Down</button>
             </li>;
-        let answersList = question.answers.map(mapFunction);
+        let answersList = question.answers.map(mapFunction);*/
+        let content = <p>Loading.... </p>;
 
-        return (
-            <>
-                <h2>{question.title}</h2>
-                <h3>Answers:</h3>
-                <ul>
-                    {answersList}
-                </ul>
-                <PostAnswer path="/" submit={(answer) => this.addAnswer(answer, question)}></PostAnswer>
-                <br/><br/>
+        if(question) {
+            content = (
+                <>
+                    <h2>{question.title}</h2>
+                    <h3>Answers:</h3>
+                    <ul>
+                        {question.answers.map(a, index => (
+                            <li key={index}>
+                                {a.text}
+                                <button onClick={() => this.upVote(a._id, question)} style={{marginLeft: "2vw"}}>Up</button>
+                                <span style={{margin: "0 2vw"}}>{a.vote}</span>
+                                <button onClick={() => this.downVote(a._id, question)}>Down</button>
+                            </li>
+                        ))}
+                    </ul>
+                    <PostAnswer path="/" submit={(answer) => this.addAnswer(answer, question)}></PostAnswer>
+                    <br/><br/>
 
 
-                <Link to="/">Back to questions</Link>
-            </>
-        );
+                    <Link to="/">Back to questions</Link>
+                </>
+            )
+        }
+
+        return content;
+{/*        <>
+            <h2>{question.title}</h2>
+            <h3>Answers:</h3>
+            <ul>
+                {answersList}
+            </ul>
+            <PostAnswer path="/" submit={(answer) => this.addAnswer(answer, question)}></PostAnswer>
+            <br/><br/>
+
+
+            <Link to="/">Back to questions</Link>
+        </>*/}
     }
 }
 
